34398609cf8117a805ba00c1c8ee8b93d3737b98
lecrabe <lecrabe@users.noreply.github.com>
2017-08-13 14:43:40 +0000

    dropbox link
    dropbox link

--- scripts_bhutan/ex1_combine_lc_maps/script0_download_ws_data.R
+++ scripts_bhutan/ex1_combine_lc_maps/script0_download_ws_data.R
@@ -15,8 +15,7 @@
 
 ############### DOWNLOAD WORKSHOP DATA
   setwd("~/btn_ws_20170814/")
-  system("wget https://download.wetransfer.com/eu2/82fb49b5c5d41fd198a97b10bc42033620170813041404/gis_data_bhutan.zip?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1bmlxdWUiOiI4MmZiNDliNWM1ZDQxZmQxOThhOTdiMTBiYzQyMDMzNjIwMTcwODEzMDQxNDA0IiwicHJvZmlsZSI6ImV1MiIsImZpbGVuYW1lIjoiZ2lzX2RhdGFfYmh1dGFuLnppcCIsImVzY2FwZWQiOiJmYWxzZSIsImV4cGlyZXMiOjE1MDI2MDMzNTIsImNhbGxiYWNrIjoie1wiZm9ybWRhdGFcIjp7XCJhY3Rpb25cIjpcImh0dHBzOi8vYXBpLndldHJhbnNmZXIuY29tL2FwaS92MS90cmFuc2ZlcnMvODJmYjQ5YjVjNWQ0MWZkMTk4YTk3YjEwYmM0MjAzMzYyMDE3MDgxMzA0MTQwNC9yZWNpcGllbnRzLzBlMTUzMDJjNTcyZWMxZGIxMDY3NDU1NzYwYjk3MjdjMjAxNzA4MTMwNDE0MDRcIn0sXCJmb3JtXCI6e1wic3RhdHVzXCI6W1wicGFyYW1cIixcInN0YXR1c1wiXSxcImRvd25sb2FkX2lkXCI6XCIyNzUwMDM4Mjk1XCJ9fSJ9.6X4Yx_qDY0MJalNgkNi46oO2eEIxlbLCRkG3Md1JqtE")
-  
+  system("wget https://www.dropbox.com/s/iu446cecwya1084/gis_data_bhutan.zip?dl=0")
   system("unzip gis_data_bhutan.zip" )
   
   

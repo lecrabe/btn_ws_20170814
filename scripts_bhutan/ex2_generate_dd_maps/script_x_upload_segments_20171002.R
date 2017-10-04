bc03852ef15d52029f455565b9b8b9ddca8115f9
lecrabe <remi.dannunzio@gmail.com>
2017-10-04 08:44:32 +0200

    upload and sepal issues

--- scripts_bhutan/ex2_generate_dd_maps/script_x_upload_segments_20171002.R
+++ scripts_bhutan/ex2_generate_dd_maps/script_x_upload_segments_20171002.R
@@ -23,16 +23,18 @@ segdir  <- paste0(rootdir,"segments_FREL/")
 dir.create(segdir)
 setwd(segdir)
 
-system("download_url = \"https://drive.google.com/uc?authuser\u003d0\u0026id\u003d0B48Ol_Tb6ewSX1RVcEZCRm9lWGM\u0026export\u003ddownload\" ") 
-       #-O essayons https://googledrive.com/host/0B48Ol_Tb6ewSX1RVcEZCRm9lWGM")
-system("curl -L -o retest https://drive.google.com/uc?id=0B48Ol_Tb6ewSX1RVcEZCRm9lWGM")
 
-system("wget u'https://drive.google.com/uc?authuser=0&id=0B48Ol_Tb6ewSX1RVcEZCRm9lWGM&export=download'")
-system("wget -O output https://drive.google.com/uc?authuser\u003d0\u0026id\u003d0B48Ol_Tb6ewSX1RVcEZCRm9lWGM\u0026export\u003ddownload")
+######### G_DRIVE SOLUTION THROUGH
+######### http://olivermarshall.net/how-to-upload-a-file-to-google-drive-from-the-command-line/
+  
+# system("download_url = \"https://drive.google.com/uc?authuser\u003d0\u0026id\u003d0B48Ol_Tb6ewSX1RVcEZCRm9lWGM\u0026export\u003ddownload\" ") 
+# # -O essayons https://googledrive.com/host/0B48Ol_Tb6ewSX1RVcEZCRm9lWGM")
+# system("curl -L -o retest https://drive.google.com/uc?id=0B48Ol_Tb6ewSX1RVcEZCRm9lWGM")
+# 
+# system("wget u'https://drive.google.com/uc?authuser=0&id=0B48Ol_Tb6ewSX1RVcEZCRm9lWGM&export=download'")
+# system("wget -O output https://drive.google.com/uc?authuser\u003d0\u0026id\u003d0B48Ol_Tb6ewSX1RVcEZCRm9lWGM\u0026export\u003ddownload")
 
 system(sprintf("echo 4/SGn-wOSw1u0Pt2kW3Sctd_4nyrYX0ZMsn2YcJWvYejc | gdrive download 0B48Ol_Tb6ewSX1RVcEZCRm9lWGM --force"))
-system("wget --save-cookies cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B48Ol_Tb6ewSX1RVcEZCRm9lWGM' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/Code: \1\n/p' >> code")
-system("wget --load-cookies cookies.txt 'https://drive.google.com/uc?export=download&confirm=9ebY&id=0B48Ol_Tb6ewSX1RVcEZCRm9lWGM' -O testagain")
 
 system("unzip segments_FREL.zip?dl=0")
 
@@ -47,6 +49,6 @@ summary(dbf)
 hist(dbf$areas)
 
 ###### Create unique ID for boundaries
-dbf <- read.dbf(paste0("boundaries_bhutan/bhutan.dbf"))
-dbf$id <- row(dbf)[,1]
-write.dbf(dbf,paste0("boundaries_bhutan/bhutan.dbf"))
+# dbf <- read.dbf(paste0("boundaries_bhutan/bhutan.dbf"))
+# dbf$id <- row(dbf)[,1]
+# write.dbf(dbf,paste0("boundaries_bhutan/bhutan.dbf"))
